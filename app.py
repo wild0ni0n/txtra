d0f7c23b5bad2abf7ef2628d7bd5826ddc68fd26
wild0ni0n <yuisuwako@gmail.com>
2023-10-10 02:00:44 +0900

    Added token extraction functionality

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import dns.resolver
+from dns import resolver
 from urllib.parse import urlparse
 from colorama import Fore
 import yaml
@@ -29,13 +29,19 @@ class Template:
             self.yaml_data = yaml.safe_load(yml)
         self.load(self.yaml_data)
 
-    def match(self, value) -> Optional[str]:
-        if self.rule['type'] == 'regex':
-            r = self.rule['regex']
-            if re.search(r[0], value):
-                return value
-            else:
-                return None
+    def match(self, value: str) -> Optional[re.Match]:
+        if self.rule["type"] == "regex":
+            m = re.search(self.rule["regex"][0], value)
+            if m:
+                return m
+        return None
+
+    def get_paramname(self) -> Optional[List[str]]:
+        if "params" in self.rule:
+            return self.rule["params"]
+        else:
+            return None
+
 
 @dataclass
 class Domain:
@@ -45,11 +51,54 @@ class Domain:
         if self.name[:4] == "http":
             o = urlparse(self.name)
             self.name = o.hostname
-    
+
     def __str__(self) -> str:
         return self.name
+
     def __repr__(self) -> str:
-        return  self.name
+        return self.name
+
+
+class TxtRecord:
+    def __init__(self, value: str) -> None:
+        self.value = value
+        self.is_matched = False
+
+    def scan(self, templates: List[Template]) -> Optional[Template]:
+        for template in templates:
+            m = template.match(self.value)
+            if m:
+                self.template = template
+                self.provider = self.template.name
+                self.category = self.template.category
+                self.is_matched = True
+                try:
+                    self.token: str = m.group("token")
+                except IndexError:
+                    self.token = ""
+                return template
+        return None
+
+
+class TxtRecords:
+    def __init__(self, domain: Domain) -> None:
+        self.domain: Domain = domain
+        self.records: List[TxtRecord] = []
+        self.is_matched = False
+
+    def resolve(self) -> List[TxtRecord]:
+        answers = resolver.resolve(self.domain.name, "TXT")
+        for rdata in answers:
+            for data in rdata.strings:
+                self.records.append(TxtRecord(data.decode("utf-8")))
+        return self.records
+
+    def scan(self, templates: List[Template]):
+        for record in self.records:
+            record.scan(templates)
+
+    def __iter__(self):
+        yield from self.records
 
 
 def load_templates() -> List[Template]:
@@ -72,46 +121,50 @@ def detect_provider_from_txt(txt, templates: List[Template]) -> Optional[Templat
 def stdout_mode(domains: List[Domain]):
     print("[INF] Check {} domains".format(len(domains)))
     for domain in domains:
+        records = TxtRecords(domain=domain)
         try:
-            answers = dns.resolver.resolve(domain.name, "TXT")
-            for rdata in answers:
-                for data in rdata.strings:
-                    txtval = data.decode('utf-8')
-                    t = detect_provider_from_txt(txtval, templates)
-                    if t is not None:
-                        print(Fore.YELLOW +
-                              "[{}]".format(domain) + Fore.BLUE + " [{}] ".format(t.name) + Fore.YELLOW + "{}".format(txtval))
-                    else:
-                        print(Fore.YELLOW +
-                              "[{}] {} ".format(domain, txtval))
-
-        except:
+            records.resolve()
+        except resolver.NoAnswer:
             continue
-
-
-def load_list(file, limit=None):
-    with open(file, "r") as f:
-        lines = f.readlines()
-
-    p_lines = []
-    try:
-        for line in lines[:limit]:
-            if line[:4] == "http":
-                o = urlparse(line)
-                p_lines.append(o.hostname)
+        records.scan(templates=templates)
+        for record in records:
+            if record.is_matched:
+                if record.token != "":
+                    print(
+                        Fore.YELLOW
+                        + "[{}]".format(records.domain)
+                        + Fore.BLUE
+                        + " [{}] ".format(record.provider)
+                        + Fore.CYAN
+                        + " [token={}] ".format(record.token)
+                        + Fore.YELLOW
+                        + "{}".format(record.value)
+                    )
+                else:
+                    print(
+                        Fore.YELLOW
+                        + "[{}]".format(records.domain)
+                        + Fore.BLUE
+                        + " [{}] ".format(record.provider)
+                        + Fore.YELLOW
+                        + "{}".format(record.value)
+                    )
             else:
-                p_lines.append(line)
-    except:
-        print("Some domains cannot be parsed.")
+                print(Fore.YELLOW + "[{}] {} ".format(records.domain, record.value))
 
-    return p_lines
 
 def argparse_setup() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser()
-    parser.add_argument('-d', '--domain', help='Specify domain')
-    parser.add_argument('-f', '--file', help='Specify domains file', type=argparse.FileType("r", encoding="utf-8"))
-    #parser.add_argument('-o', 'Specify output file')
+    parser.add_argument("-d", "--domain", help="Specify domain")
+    parser.add_argument(
+        "-f",
+        "--file",
+        help="Specify domains file",
+        type=argparse.FileType("r", encoding="utf-8"),
+    )
+    # parser.add_argument('-o', 'Specify output file')
     return parser
+
 
 if __name__ == "__main__":
     parser = argparse_setup()
@@ -123,8 +176,8 @@ if __name__ == "__main__":
         domain = Domain(args.domain)
         stdout_mode([domain])
         exit()
-    
-    if args.file != None:
+
+    if args.file is not None:
         lines = args.file.read().splitlines()
         domains = list(map(lambda v: Domain(v), lines))
         print(domains)
