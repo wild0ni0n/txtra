a40677131859253dceeef5a9d16989aba22ec48a
wild0ni0n <yuisuwako@gmail.com>
2024-08-04 17:03:10 +0900

    include provider file

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup
 import os
 
+def readme():
+    return open('README.md', 'r', encoding='utf-8').read()
+
 def strip_comments(l):
     return l.split('#', 1)[0].strip()
 
@@ -12,6 +15,9 @@ setup(
     name='txtra',
     version='0.0.1',
     install_requires=reqs('requirements.txt'),
+    load_description=readme(),
+    load_description_content_type='text/markdown',
+    include_package_data=True,
     entry_points={
         'console_scripts':[
             'txtra = txtra.__main__:main',
