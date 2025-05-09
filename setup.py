a592e071f36748410c05ffdfb47fb4ed2aba447a
wild0ni0n <yuisuwako@gmail.com>
2025-05-10 01:28:56 +0900

    パッケージング用に調整

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup
 import os
 
 def readme():
-    return open('README.md', 'r', encoding='utf-8').read()
+    with open('README.md', 'r', encoding='utf-8') as f:
+        return f.read()
 
 def strip_comments(l):
     return l.split('#', 1)[0].strip()
@@ -12,15 +13,7 @@ def reqs(*f):
         return [strip_comments(L) for L in f if strip_comments(L)]
 
 setup(
-    name='txtra',
-    version='0.0.1',
+    long_description=readme(),
     install_requires=reqs('requirements.txt'),
-    load_description=readme(),
-    load_description_content_type='text/markdown',
     include_package_data=True,
-    entry_points={
-        'console_scripts':[
-            'txtra = txtra.__main__:main',
-        ],
-    },
 )
